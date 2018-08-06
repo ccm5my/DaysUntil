0b9369f5b4dfba5cf794e1b10efbc57fd8751b7c
Cameron Moore <cmoore@captechventures.com>
2018-08-06 15:43:31 -0400

    Wrote function that calculates days until my next Paycheck

--- ViewController.swift
+++ ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var daysUntilPaycheck: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func daysUntil()->Int {
+        let date = Date()
+        let calendar = Calendar.current
+        let day: Int = calendar.component(.day,from: date)
+        
+        if (day == 5 || day == 20) {
+            return 0;
+        }
+        if day >= 1 && day < 5 {
+            return 5-day
+        }
+        if day>5 && day<20 {
+            return 20-day
+        }
+        if day<=31 && day>20 {
+            let range = calendar.range(of: .day, in: .month, for: date)!
+            let numDaysInMonth = range.count
+            return 5-(numDaysInMonth-day)
+        }
+        return 0;
     }
-
-
+        
+            
+        
+   
 }
+
 
